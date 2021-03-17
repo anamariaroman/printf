01d03064e433cadc174acd3fc7db6dec9442bbb6
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-17 02:26:33 +0000

    holberton.h con correcion en el nombre de una funcion

---
+++ holberton.h
@@ -0,0 +1,28 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct pr - Struct pr
+ *
+ * @s: Data
+ * @n: format define
+ */
+typedef struct pr
+{
+	char *s;
+	int (*n)(va_list);
+} pr_t;
+
+int _putchar(char c);
+int (*print_format(const char *format))(va_list);
+int _printf(const char *format, ...);
+int print_c(va_list print);
+int print_s(va_list print);
+int Percent_sign(__attribute__((unused)) va_list print);
+int print_i(va_list print);
+
+
+#endif /* _HOLBERTON_H */
