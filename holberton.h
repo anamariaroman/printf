cae8b5621cb46cdd98e36d20de8c88fdddf225e6
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-03-17 02:04:20 +0000

    holberton.h con correcion en el nombre de una funcion

--- holberton.h
+++ holberton.h
@@ -20,9 +20,9 @@ int _putchar(char c);
 int (*print_format(const char *format))(va_list);
 int _printf(const char *format, ...);
 int print_c(va_list print);
-int print_s(va_list print_s);
+int print_s(va_list print);
 int Percent_sign(__attribute__((unused)) va_list print);
-int print_i(va_list print_i);
+int print_i(va_list print);
 
 
 #endif /* _HOLBERTON_H */
